c06278f97cb339b489372109991b4e7093f371a8
mauriciojuba <mauriciojuba@outlook.com>
2016-06-13 01:18:51 -0300

    FirstCommit

---
+++ Assets/Scripts/EnemyScripts/Enemy_Hero/Enemy_Hero_01_Eye.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Hero_01_Eye : MonoBehaviour {
+
+    public bool playerNear, minionNear, towerNear;
+    public GameObject closestMinion;
+
+    //trocar para as tags usadas na cena original
+	void OnTriggerEnter(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            playerNear = true;
+        }
+        if (hit.CompareTag("PlayerMinion") && !playerNear && !towerNear)
+        {
+            minionNear = true;
+            closestMinion = hit.gameObject;
+
+        }
+        if (hit.CompareTag("PlayerTower")&& !playerNear)
+        {
+            towerNear = true;
+        }
+    }
+    void OnTriggerExit(Collider hit)
+    {
+        if (hit.CompareTag("Player"))
+        {
+            playerNear = false;
+        }
+        if (hit.CompareTag("PlayerMinion"))
+        {
+            minionNear = false;
+        }
+        if (hit.CompareTag("PlayerTower"))
+        {
+            towerNear = false;
+        }
+    }
+}
