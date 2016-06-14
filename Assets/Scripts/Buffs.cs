c4c15bbe7eca3b1bee35e226569d780681482e20
victortomioka <vtomioka@gmail.com>
2016-06-14 04:03:57 -0300

    buffs2

--- Assets/Scripts/Buffs.cs
+++ Assets/Scripts/Buffs.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Buffs : MonoBehaviour {
 
-	public static void buffTroops(){
+	public static void maxHealthBuff(List<GameObject> targets, float buff){
+
+	}
+
+	public static void damageBuff(List<GameObject> targets, float buff){
 
 	}
 }
