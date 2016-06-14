f39ae37eb9326ed10fa493eb665e0f9e6c916d77
victortomioka <vtomioka@gmail.com>
2016-06-14 02:03:02 -0300

    bases

---
+++ Assets/VICTOR_InimigoTurretBase/Scripts/Base.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Base : MonoBehaviour {
+
+	public enum baseTeam{ally,enemy};
+	public baseTeam team;
+	public GameObject[] enemies;
+	public Transform spawnPoint;
+	public float minSpawnTime = 1f;
+	public float maxSpawnTime = 10f;
+	public int squadronSize = 1;
+	GameObject enMaster;
+
+	void Start () 
+	{
+		this.GetComponent<LivingEntity>().onDeath+=baseDestroyed;
+		Invoke ("SpawnEnemy",Random.Range (minSpawnTime,maxSpawnTime));
+		enMaster = new GameObject();
+		enMaster.name = "enemies";
+	}
+
+	void SpawnEnemy () 
+	{
+		for (int i = 0; i < squadronSize-1; i++) {
+			GameObject newEnemy = Instantiate (enemies [Random.Range (0, enemies.Length)], spawnPoint.position, spawnPoint.rotation) as GameObject;
+			newEnemy.transform.parent = enMaster.transform;
+			newEnemy.GetComponent<LivingEntity>().onDeath+=enemyDead;
+		}
+
+		Invoke ("SpawnEnemy",Random.Range (minSpawnTime,maxSpawnTime)); 
+	}
+
+	void enemyDead(){
+		//do something;
+	}
+
+	void baseDestroyed(){
+		switch(team){
+		case(baseTeam.ally):break;
+		case(baseTeam.enemy):break;
+		}
+	}
+}
--- Assets/VICTOR_InimigoTurretBase/Scripts/Turret.cs
+++ Assets/VICTOR_InimigoTurretBase/Scripts/Turret.cs
@@ -15,7 +15,7 @@ public class Turret : MonoBehaviour {
 	float currentCd;
 
 	public void OnTriggerEnter(Collider c){
-		if(layers == (layers | (1 << c.gameObject.layer))){
+		if(layers == (layers | (1 << c.gameObject.layer))){ //operacao bitwise pra descobrir se o collider c é da mesma layer da layermask
 			target = c.transform;
 		}
 	}
@@ -32,7 +32,7 @@ public class Turret : MonoBehaviour {
 		}
 	}
 		
-	void aimAt(){
+	void aimAt(){ // mira no alvo
 		Quaternion targetRot = Quaternion.LookRotation (target.position - rootBone.position);
 		rootBone.transform.rotation = Quaternion.RotateTowards (rootBone.rotation, targetRot, angularVelocity * Time.deltaTime);
 		if(currentCd==0){
@@ -40,22 +40,23 @@ public class Turret : MonoBehaviour {
 		}
 	}
 
-	void shoot(){
+	void shoot(){ // dispara se cooldown <= 0;
 		GameObject bul = Instantiate(bullet, muzzle.position, muzzle.rotation) as GameObject;
 		currentCd = cooldown;
 		StartCoroutine(coolDown());
 	}
 
 	void OnDrawGizmosSelected(){
-		Gizmos.DrawWireSphere(transform.position,range);
+		Gizmos.DrawWireSphere(transform.position,range); //desenha uma esfera pra indicar o alcance da turret
 	}
 
+	//realiza o cooldown da turret
 	IEnumerator coolDown(){
 		while(currentCd>0){
-			currentCd-=Time.deltaTime;
-			yield return null;
-		}
-		if(currentCd<0){
+			currentCd-=Time.deltaTime; //abaixo o cooldown atual
+			yield return null; //força a coroutine a reiniciar do começo, mas mantêm os valores alterados (funciona como um nextStep)
+		} 
+		if(currentCd<0){ //se o cooldown atual for menor que zero, zerá-lo
 			currentCd = 0;
 		}
 	}
